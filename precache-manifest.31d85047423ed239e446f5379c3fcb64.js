c633f5a5f3f96faf6c56399fe66dfbcfd567a178
“ilroman” <il.roman@ukr.net>
2020-04-26 14:35:23 +0300

    Updates

---
+++ precache-manifest.31d85047423ed239e446f5379c3fcb64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec88bae67fd885378edc3c59e0e661f7",
+    "url": "/Todo-typescript/index.html"
+  },
+  {
+    "revision": "e2bafc53e74ac8888c97",
+    "url": "/Todo-typescript/static/css/main.1ab91ffd.chunk.css"
+  },
+  {
+    "revision": "bc320055074f31a297c0",
+    "url": "/Todo-typescript/static/js/2.d12fc908.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Todo-typescript/static/js/2.d12fc908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2bafc53e74ac8888c97",
+    "url": "/Todo-typescript/static/js/main.d67c8b1f.chunk.js"
+  },
+  {
+    "revision": "5610919ddb84dff76e29",
+    "url": "/Todo-typescript/static/js/runtime-main.bb2a36d1.js"
+  }
+]);
